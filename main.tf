6697a0ae67f39c91489cdf4e70da01298972e9f6
shalaermal <ermalshala10@gmail.com>
2026-02-28 11:01:00 -0500

    fixing config on main

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+# Latest Amazon Linux 2023 AMI(x86_64) via SSM
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+}
+
+# Pick a default subnet from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+}
+
 resource "aws_instance" "web" {
   ami                    = data.aws_ssm_parameter.al2023.value
   instance_type          = var.instance_type
@@ -10,26 +24,12 @@ resource "aws_instance" "web" {
   Name = "${var.project}-web"
   }
 
-# Latest Amazon Linux 2023 AMI(x86_64) via SSM
-data "aws_ssm_parameter" "al2023" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
-}
-
-# Pick a default subnet from the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
   # Minimal Python web server serving "Hello World"
   user_data = <<EOF
   #!/bin/bash
-  dnf -y install python3
+  dnf install -y python3
   echo "Hello World" > /home/ec2-user/index.html
   cd /home/ec2-user
-  nohup python3 -m http.server 80 --bind 0.0.0.0 >/var/log/web.log 2>&1 &
+  nohup sudo python3 -m http.server 80 >/var/log/web.log 2>&1 &
   EOF
-
 }
